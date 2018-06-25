14dff531791acc69fcd0c5bf0838ae7793d4b569
haw <haw358linux@163.com>
2018-06-25 14:08:05 +0800

    springcloud service consumer: feign

---
+++ src/main/java/top/haw/feign/SchedualServiceHi.java
@@ -0,0 +1,17 @@
+package top.haw.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by haw on 18-6-25.
+ */
+@Service
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
